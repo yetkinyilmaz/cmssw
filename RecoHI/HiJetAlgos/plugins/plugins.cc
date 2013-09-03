f8476f0e3f27be3c59661c6b7248f7b3c8e943a5
Yetkin <yetkin.yilmaz@cern.ch>
2013-09-03 17:13:36 +0200

    Fixes for missing files

--- RecoHI/HiJetAlgos/plugins/plugins.cc
+++ RecoHI/HiJetAlgos/plugins/plugins.cc
@@ -3,6 +3,7 @@
 #include "RecoHI/HiJetAlgos/interface/JetOffsetCorrector.h"
 #include "RecoHI/HiJetAlgos/interface/ParametrizedSubtractor.h"
 #include "RecoHI/HiJetAlgos/interface/ReflectedIterator.h"
+#include "RecoHI/HiJetAlgos/interface/VoronoiSubtractor.h"
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -13,7 +14,7 @@ DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,JetOffsetCorrector,"JetOffsetCorrector
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,MultipleAlgoIterator,"MultipleAlgoIterator");
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,ParametrizedSubtractor,"ParametrizedSubtractor");
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,ReflectedIterator,"ReflectedIterator");
-
+DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,VoronoiSubtractor,"VoronoiSubtractor");
 
 
 
