74697da5694834d1db5288af298566c52918524c
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2013-09-06 16:06:09 +0200

    Update classes.h

--- DataFormats/HeavyIonEvent/src/classes.h
+++ DataFormats/HeavyIonEvent/src/classes.h
@@ -1,6 +1,7 @@
 #include "DataFormats/HeavyIonEvent/interface/Centrality.h"
 #include "DataFormats/HeavyIonEvent/interface/EvtPlane.h"
 #include "DataFormats/HeavyIonEvent/interface/HeavyIon.h"
+#include "DataFormats/HeavyIonEvent/interface/VoronoiBackground.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace { 
