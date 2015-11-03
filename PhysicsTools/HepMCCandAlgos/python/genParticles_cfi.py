bc70189fcc526dbb74aa632e82a266d47d5b96ea
Yetkin <yetkin.yilmaz@cern.ch>
2015-11-03 16:12:46 +0100

    put default inputtag for mix

--- PhysicsTools/HepMCCandAlgos/python/genParticles_cfi.py
+++ PhysicsTools/HepMCCandAlgos/python/genParticles_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 genParticles = cms.EDProducer("GenParticleProducer",
     saveBarCodes = cms.untracked.bool(True),
     src = cms.InputTag("generator"),
+    mix = cms.string("mix"), # inactive unless useCF == True
     abortOnUnknownPDGCode = cms.untracked.bool(False)
 )
 
