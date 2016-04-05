2858fcc3509cb4eedeca5c6e7f883ccd1c6526d0
Marta <marta.verweij@cern.ch>
2016-04-05 16:09:53 +0200

    filter subid=0 particles for subjet clustering at gen level

--- HeavyIonsAnalysis/JetAnalysis/python/GenJetSequence.py
+++ HeavyIonsAnalysis/JetAnalysis/python/GenJetSequence.py
@@ -2,7 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoHI.HiJetAlgos.HiGenJets_cff import *
 from RecoJets.Configuration.GenJetParticles_cff import *
+from RecoHI.HiJetAlgos.HiSignalParticleProducer_cfi import hiSignalGenParticles
 from HeavyIonsAnalysis.JetAnalysis.akSoftDrop4GenJets_cfi import akSoftDrop4GenJets
+akSoftDrop4GenJets.src            = cms.InputTag("hiSignalGenParticles")
 
 akSoftDrop1GenJets = akSoftDrop4GenJets.clone(rParam = 0.1)
 akSoftDrop2GenJets = akSoftDrop4GenJets.clone(rParam = 0.2)
@@ -12,6 +14,7 @@ akSoftDrop6GenJets = akSoftDrop4GenJets.clone(rParam = 0.6)
 
 akHiGenJets = cms.Sequence(
     genParticlesForJets +
+    hiSignalGenParticles +
     ak1HiGenJets +
     ak2HiGenJets +
     ak3HiGenJets +
